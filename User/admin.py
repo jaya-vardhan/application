ccca62bbcb0b527bdb29fc566951681452883f6f
jaya-vardhan <93918936+jaya-vardhan@users.noreply.github.com>
2021-11-11 23:01:30 +0530

    Add files via upload

---
+++ User/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['first_name']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['user']
